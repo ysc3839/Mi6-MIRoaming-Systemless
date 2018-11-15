e40c926ec468b290f9ee86f9c4ee929694321ef2
Richard Yu <yurichard3839@gmail.com>
2018-11-15 19:46:09 +0800

    Exclude *.jar and vdexExtractor from final zip.

--- build.sh
+++ build.sh
@@ -36,4 +36,4 @@ find -type d -exec chmod 0755 {} +
 find -type f -exec chmod 0644 {} +
 
 version=$(grep -Po "version=\K.*" module.prop)
-zip -r -x *.git* build.sh *.txt *.py *.zip *.yml -y -9 Mi6-MIRoaming-Systemless-$version.zip . || exit 1
+zip -r -x *.git* build.sh *.txt *.py *.zip *.yml *.jar vdexExtractor -y -9 Mi6-MIRoaming-Systemless-$version.zip . || exit 1
